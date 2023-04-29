0ff0ff6841848eaa2e007f32c6f29a441a0a6c20
Jaqueline <jaquelinenacarate@gmail.com>
2023-04-29 20:01:07 +0000

    Save urlID-longURL to urlDatabase and redirect to /urls/:id

--- express_server.js
+++ express_server.js
@@ -5,19 +5,17 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 // Func to return 6 random alphanumeric characters
-function generateRandomString(length) {
-    let result = "";
-    const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
-    }
-    return result;
-}
+const generateRandomString = function(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
 
-generateRandomString(6);
+// generateRandomString(6);
 
 // Middleware to translate, or parse the body from the POST request
 app.use(express.urlencoded({ extended: true }));
@@ -28,17 +26,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 // Route handler for all "/urls"
 app.get("/urls", (req, res) => {
@@ -47,22 +45,30 @@ app.get("/urls", (req, res) => {
   // the variable (urls) to access the data
   // within the template
   const templateVars = {
-    urls: urlDatabase
+    urls: urlDatabase,
   };
   res.render("urls_index", templateVars);
 });
 
-
 // Route to show new form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-})
+});
 
 // Route to receive form submission
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const newURL = req.body.longURL;
+
+  // New id (shortURL)
+  const newID = generateRandomString(6);
+
+  // Add new id (shortURL)-longURL key-value pairs to DB
+  urlDatabase[newID] = newURL;
+
+  // Use the NEW route to show/view the URL created
+  res.redirect(`/urls/${newID}`);
 });
+
 
 // Render/ show information about a single URL
 app.get("/urls/:id", (req, res) => {
@@ -75,9 +81,6 @@ app.get("/urls/:id", (req, res) => {
 
   res.render("urls_show", templateVars);
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);

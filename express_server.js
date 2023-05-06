e5b9d47a80617234c419dd68bdc2db4507c02039
Jaqueline <jaquelinenacarate@gmail.com>
2023-05-06 01:32:25 +0000

    Pass new urlDatabase to urls_index template

--- express_server.js
+++ express_server.js
@@ -36,8 +36,14 @@ app.set("view engine", "ejs");
 
 // Data to show on the URLs page
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "user2RandomID",
+  },
 };
 
 // Object to store users
@@ -185,7 +191,7 @@ app.get("/urls", (req, res) => {
   // within the template
   const templateVars = {
     user,
-    urls: urlDatabase,
+    urlDatabase,
   };
   res.render("urls_index", templateVars);
 });

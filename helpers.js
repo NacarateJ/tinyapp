c2e839c79b1bb54545d5f313c616f039bd7f1e09
Jaqueline <jaquelinenacarate@gmail.com>
2023-05-06 22:38:08 +0000

    Add all helper functions
    Added all helper functions to helper.js file.

--- helpers.js
+++ helpers.js
@@ -1,3 +1,18 @@
+/////////////////////////////////////////////////////////////////////
+// Helper Functions
+/////////////////////////////////////////////////////////////////////
+
+// Func to return 6 random alphanumeric characters
+const generateRandomString = function (length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
 // Helper function to check for existing users email
 const getUserByEmail = function (email, usersDatabase) {
   for (const userID in usersDatabase) {
@@ -10,4 +25,27 @@ const getUserByEmail = function (email, usersDatabase) {
   return undefined;
 };
 
-module.exports = { getUserByEmail };
+// Ckeck if the user have URLs to show 
+const urlsForUser = function (id) {
+  const userURLs = {};
+  let foundURL = false;
+
+  for (const url in urlDatabase) {
+    if (id === urlDatabase[url].userID) {
+      userURLs[url] = urlDatabase[url];
+      foundURL = true;
+    }
+  }
+
+  if (!foundURL) {
+    return "Start creating your short URLs!";
+  }
+
+  return userURLs;
+};
+
+module.exports = {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser
+};

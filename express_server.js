b4d864373c8be55e6d6f3504077cd688f82997c1
Jaqueline <jaquelinenacarate@gmail.com>
2023-05-06 16:33:37 +0000

    Install and require cookie-parser

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@
 const express = require("express");
 const request = require("request");
 const bcrypt = require("bcryptjs");
-const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 
 /////////////////////////////////////////////////////////////////////
 // Initialization

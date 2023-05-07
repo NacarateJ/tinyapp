668968345cc2daa1bce6c82d11d3fe555d545380
Jaqueline <jaquelinenacarate@gmail.com>
2023-05-07 07:39:01 +0000

    Write tests for helper functions
    Wrote tests for the other 2 helper functios generateRandomString and urlsForUser.

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,37 @@
 const { assert } = require("chai");
 
-const { getUserByEmail } = require("../helpers.js");
+const {
+  generateRandomString,
+  getUserByEmail,
+  urlsForUser,
+} = require("../helpers.js");
+
+/////////////////////////////////////////////////////////////////////
+// Tests for generateRandomString
+/////////////////////////////////////////////////////////////////////
+
+describe("generateRandomString", function () {
+  it("should return a string of 6 random alphanumeric characters", function () {
+    const length = 6;
+    const result = generateRandomString(length);
+    assert.strictEqual(result.length, length);
+  });
+
+  it("should only contain alphanumeric characters", function () {
+    const result = generateRandomString(6);
+    assert.match(result, /^[a-zA-Z0-9]+$/); //'QQxf5Y'
+  });
+
+  it("should return different strings each time it is called", function () {
+    const result1 = generateRandomString(6);
+    const result2 = generateRandomString(6);
+    assert.notStrictEqual(result1, result2);
+  });
+});
+
+/////////////////////////////////////////////////////////////////////
+// Tests for getUserByEmail
+/////////////////////////////////////////////////////////////////////
 
 const testUsers = {
   userRandomID: {
@@ -25,5 +56,32 @@ describe("getUserByEmail", function () {
   it("should return undefined for a non-existent email", function () {
     const user = getUserByEmail("nonexistent@example.com", testUsers);
     assert.isUndefined(user);
+  });
+});
+
+/////////////////////////////////////////////////////////////////////
+// Tests for urlsForUser
+/////////////////////////////////////////////////////////////////////
+
+describe("urlsForUser", function () {
+   const testUrlDatabase = {
+     abcd: { longURL: "http://example.com", userID: "user1" },
+     efgh: { longURL: "http://example.org", userID: "user2" },
+     ijkl: { longURL: "http://example.net", userID: "user1" },
+   };
+
+  it("should return an object containing the URLs associated with the given user ID", function () {
+    const result = urlsForUser("user1", testUrlDatabase);
+    const expected = {
+      abcd: { longURL: "http://example.com", userID: "user1" },
+      ijkl: { longURL: "http://example.net", userID: "user1" },
+    };
+    assert.deepEqual(result, expected);
+  });
+
+  it("should return a message string when the user ID does not own any URLs", function () {
+    const result = urlsForUser("user3", testUrlDatabase);
+    const expected = "Start creating your short URLs!";
+    assert.deepEqual(result, expected);
   });
 });
